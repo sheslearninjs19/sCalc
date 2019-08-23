fcb372f79655c71f9a6078f5c107ddfafa01d98e
Ivan Astudillo <ivankansasc@gmail.com>
2019-08-22 19:16:59 -0500

    nice

---
+++ main.js
@@ -0,0 +1,26 @@
+var x = document.getElementById("num1").innerHTML;
+var y  = document.getElementById("num2").innerHTML;
+var z  = document.getElementById("num3").innerHTML;
+
+function add(x, y) {
+z = x + y;
+document.getElementById("result").innerHTML=z;
+}
+
+function sub(x, y);{
+
+z= x-y;
+ document.getElementById("result").innerHTML=z;
+}
+
+function div(x, y) {
+z = x / y;
+document.getElementById("result").innerHTML=z;
+}
+function multiply(x, y) {
+    z = x * y;
+    document.getElementById("result").innerHTML=z;
+    }
+    
+    
+    
